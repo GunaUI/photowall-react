c3d7a608f6d86941278da6965bf667216280cdb4
gunm <gunm@cisco.com>
2018-05-19 15:56:06 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom'
-const task = ['Scub bugs',"Resolve the existing issues","Make a report"];
-const element = 
-    <div>
-        <h1>Tasks</h1>
-        <ol>
-            {task.map((task , index) => <li key={index} >{task}</li>)}
-        </ol>
-    </div>
 
-ReactDom.render(element, document.getElementById('root'));
+class List extends Component {
+    render(){
+        return (
+                <ol>
+                    {this.props.tasks.map((task , index) => <li key={index} >{task}</li>)}
+                </ol>
+                );
+    }
+}
+class Title extends Component {
+    render(){
+        return (
+                <h1>{this.props.headerTitle}</h1>
+                );
+    }
+}
+
+class Main extends Component {
+    render(){
+        return (
+                <div>
+                    <Title headerTitle={"Todo List"}/>
+                    <List tasks={['1Scub bugs',"2Resolve the existing issues","3Make a report"]}/>
+                    <List tasks={['4Scub bugs',"5Resolve the existing issues","6Make a report"]}/>
+                </div>
+                );
+    }
+}
+ReactDom.render(<Main/>, document.getElementById('root'));
