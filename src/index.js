c39ff6ecd2531243dbaf1cd706a3ac28ad391f1a
gunm <gunm@cisco.com>
2018-05-19 13:43:01 +0530

    JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 const task = ['Scub bugs',"Resolve the existing issues","Make a report"];
-const element = React.createElement('ol', null , task.map(
-                                                            (task, index) =>  React.createElement('li',{key: index},task)
-
-                                                        )
-                                    );
+const element = 
+    <div>
+        <h1>Tasks</h1>
+        <ol>
+            {task.map((task , index) => <li key={index} >{task}</li>)}
+        </ol>
+    </div>
 
 ReactDom.render(element, document.getElementById('root'));
